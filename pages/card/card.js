f2db7f2628953a2c6193e99123d65a5f5378994a
hisa11 <hisahisa1120@outlook.jp>
2024-12-14 01:44:33 +0900

    名刺追加

--- pages/card/card.js
+++ pages/card/card.js
@@ -38,5 +38,4 @@ document.addEventListener('DOMContentLoaded', () => {
         document.body.scrollTop = 0; // For Safari
         document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     });
-    
 });
